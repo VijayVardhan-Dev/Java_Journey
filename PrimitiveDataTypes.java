afbcef725c24f447ae9055006d904e6158f5cc98
Vardhan Keerthi <vardhankeerthi.dev@gmail.com>
2026-09-21 14:49:01 +0530

    Add PrimitiveDataTypes class with default values

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+class DefaultValues{
+    static byte by;
+    static short s;
+    static long l;
+    static int a;
+    static double b;
+    static float f;
+    static char c;
+    static boolean bool;
+
+}
+
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args){
+        System.out.println(DefaultValues.a);
+        System.out.println(DefaultValues.b);
+        System.out.println(DefaultValues.f);
+        System.out.println(DefaultValues.c);
+        System.out.println(DefaultValues.bool);
+        System.out.println(DefaultValues.by);
+        System.out.println(DefaultValues.s);
+        System.out.println(DefaultValues.l);
+    }
+}
